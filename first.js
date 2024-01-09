db423b27ff109b00a900a6871f4f54d7c85ada5a
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-01-09 11:23:51 +0530

    commit

--- first.js
+++ first.js
@@ -1,3 +1,10 @@
 console.log("Hello World");
 // the code will run on the console of the browser
 console.log("hello");
+age = 24;
+console.log(age);
+fullName = "Arpan"
+console.log(fullName);
+//var- variable can be redeclared and updated and has a global scope
+//let- variable cannot be redeclared but can be updated. It has a block scope
+//const- variable cannot be re-declared or updated. Has a block scope
